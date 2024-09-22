148b8db512a72c2f11fbade8e8f9f0f6b253e024
Tereaslle <68232265+Tereaslle@users.noreply.github.com>
2024-09-22 18:16:12 +0800

    Update classification.py

--- question1/classification.py
+++ question1/classification.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
+use_fft = True
+
 # 定义一个函数来执行FFT
 def perform_fft(row):
     # 'row'是DataFrame中的一行
@@ -17,7 +19,7 @@ def perform_fft(row):
 
 if __name__ == '__main__':
     # 定义文件路径
-    file_path = r"../appendix_1_m1.csv"
+    file_path = r"../appendix1_all.csv"
 
     # pandas.read_excel('file path')读取Excel文件
     # pandas.read_csv('file path')读取csv文件
@@ -29,23 +31,27 @@ if __name__ == '__main__':
                        "0（磁通密度B，T）": "0"}, inplace=True)
 
     # # XGBoost需要词嵌入
-    # df["励磁波形"].replace({'正弦波': 0, '三角波': 1, '梯形波': 2}, inplace=True)
-    # df["励磁波形"].astype(int)
+    df["励磁波形"].replace({'正弦波': 0, '三角波': 1, '梯形波': 2}, inplace=True)
+    df["励磁波形"].astype(int)
     # 只读取磁通密度曲线作为特征
-    features = df.iloc[:, 4:]  # 第 5 列到最后一列为磁通密度曲线
+    features = df.iloc[:, 5:]  # 第 6 列到最后一列为磁通密度曲线
     labels = df.iloc[:, 3]  # 第四列励磁波形为标签
-    features = features.to_numpy()
-    features = np.abs(np.fft.fft(features))
-    features = features[:, :features.shape[1] // 2]
+    if use_fft:
+        features = features.to_numpy()
+        features = np.abs(np.fft.fft(features))
+        features = features[:, :features.shape[1] // 40]
     # 划分训练集和测试集
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
-
+    print("------------------划分训练集完成-------------------")
     # 构建随机森林分类模型
-    model = RandomForestClassifier()
+    # model = RandomForestClassifier()
     # 构建XGBoost分类模型
-    # model = XGBClassifier()
+    model = XGBClassifier()
     # 构建贝叶斯分类器中的高斯分类器
     # model = GaussianNB()  # 垃圾
+
+    print("--------------------开始训练模型-------------------")
+
     model.fit(X_train, y_train)
 
     # 进行预测
